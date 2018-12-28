c559e25934c33e8c06cb7a3e89b7920e0bd7e499
Inskey <lyzinskey@gmail.com>
2018-12-27 18:42:47 -0800

    update

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,28 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
+
+// In development, use the browser's Redux dev tools extension if installed
+const enhancers = [];
+const isDevelopment = process.env.NODE_ENV === "development";
+if (
+  isDevelopment &&
+  typeof window !== "undefined" &&
+  window.devToolsExtension
+) {
+  enhancers.push(window.devToolsExtension());
+}
 
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ...enhancers
   )
 );
 
